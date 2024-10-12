2d7dd92072f5d56ecf68de8f8dc7d68cf9590d64
Armin Karimi <157640791+arminak6@users.noreply.github.com>
2024-10-12 09:45:52 +0200

    Lab 3

---
+++ Lab3/src/charging_node.cpp
@@ -0,0 +1,11 @@
+#include "ros/ros.h"
+#include <actionlib/client/simple_action_client.h>
+#include "std_msgs/Header.h"
+
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "charging_node");
+    // ChargingNode charging;
+    // ros::spin();
+    // return 0;
+}
---
+++ Lab3/src/robot_node.cpp
@@ -0,0 +1,16 @@
+#include "ros/ros.h"
+#include <actionlib/server/simple_action_server.h>
+#include <intro_tutorial/MyActionAction.h>
+
+
+class MyAction{
+    // protected:
+
+};
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "robot_node");
+    // RobotNode robot;
+    // ros::spin();
+    return 0;
+}
