610eed3d56bf706e41d350a300b8b07f17590f06
Armin Karimi <157640791+arminak6@users.noreply.github.com>
2024-10-18 15:58:50 +0200

    Complete MyAction class

--- Lab3/src/robot_node.cpp
+++ Lab3/src/robot_node.cpp
@@ -2,15 +2,61 @@
 #include <actionlib/server/simple_action_server.h>
 #include <intro_tutorial/MyActionAction.h>
 
+class MyAction {
+    protected:
+        ros::NodeHandle nh_;
+        actionlib::SimpleActionServer<intro_tutorial::MyActionAction> as_;
+        std::string action_name_;
+        intro_tutorial::MyActionFeedback feedback_;
+        intro_tutorial::MyActionResult result_;
+        
+    public:
+        MyAction(std::string name) :
+            as_(nh_, name, boost::bind(&MyAction::executeCB, this, _1), false),
+            action_name_(name) {
+            as_.start();
+        }
 
-class MyAction{
-    // protected:
+        ~MyAction(void) {}
 
+        void executeCB(const intro_tutorial::MyActionGoalConstPtr &goal) {
+            ros::Rate rate(1);
+            bool success = true;
+            int current_battery = 5;
+            int target_battery = goal->max_battery_level;
+            double per_second = (target_battery - current_battery) / 60.0;
+
+            while (current_battery < goal->max_battery_level) {
+                if (as_.isPreemptRequested() || !ros::ok()) {
+                    ROS_INFO("%s: Preempted", action_name_.c_str());
+                    as_.setPreempted();
+                    success = false;
+                    break;
+                }
+
+                current_battery += per_second;
+
+                if (current_battery > target_battery) {
+                    current_battery = target_battery;
+                }
+
+                feedback_.current_battery_level = current_battery;
+                as_.publishFeedback(feedback_);
+
+                rate.sleep();
+            }
+
+            if (success) {
+                result_.charging_complete = true;
+                ROS_INFO("%s: Succeeded", action_name_.c_str());
+                as_.setSucceeded(result_);
+            }
+        }
 };
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "robot_node");
-    // RobotNode robot;
-    // ros::spin();
+    MyAction my_action("my_action");
+    ros::spin();
     return 0;
 }
