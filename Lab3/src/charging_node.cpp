5eab315d354c5d24c1e07a929874a7732d54a65f
Armin Karimi <157640791+arminak6@users.noreply.github.com>
2024-10-18 18:15:07 +0200

    add charging node action

--- Lab3/src/charging_node.cpp
+++ Lab3/src/charging_node.cpp
@@ -1,11 +1,50 @@
 #include "ros/ros.h"
 #include <actionlib/client/simple_action_client.h>
-#include "std_msgs/Header.h"
-
+#include <intro_tutorial/MyActionAction.h>
+#include <intro_tutorial/MyActionResult.h>  
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "charging_node");
-    // ChargingNode charging;
-    // ros::spin();
-    // return 0;
+
+    actionlib::SimpleActionClient<intro_tutorial::MyActionAction> ac("charging_action", true);
+
+    ROS_INFO("Waiting for action server to start.");
+    ac.waitForServer(); 
+    ROS_INFO("Action server started, sending goal.");
+
+
+    intro_tutorial::MyActionGoal goal;
+    goal.max_battery_level = 80;  
+
+    ac.sendGoal(goal,
+        [](const actionlib::SimpleClientGoalState& state, const intro_tutorial::MyActionResultConstPtr& result) {
+            ROS_INFO("Charging completed with state: %s", state.toString().c_str());
+            ROS_INFO("Charging complete: %s", result->charging_complete ? "Yes" : "No");
+        },
+        []() {
+            ROS_INFO("Goal became active.");
+        },
+        [](const intro_tutorial::MyActionFeedbackConstPtr& feedback) {
+            ROS_INFO("Current battery level: %d%%", feedback->current_battery_level);
+        }
+    );
+
+
+    bool finished_before_timeout = ac.waitForResult(ros::Duration(60.0));
+    if (finished_before_timeout){
+
+        actionlib::SimpleClientGoalState state = ac.getState();
+        ROS_INFO("Action finished: %s", state.toString().c_str());
+
+        intro_tutorial::MyActionResult result = *ac.getResult();
+        ROS_INFO("Charging complete: %s", result.charging_complete ? "Yes" : "No");
+
+    }else{
+        ROS_INFO("Action did not finish before the time out.");
+    }
+    
+
+
+    ros::spin();
+    return 0;
 }
